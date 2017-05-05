fa258370ff95830aa86a8d1309acfbb1f733c6dc
Catalina Castro <catalina.cst@hotmail.es>
2017-05-05 01:09:29 -0500

    Se agregó enemigo estático (dispara)

--- BattleCity/battleCity.py
+++ BattleCity/battleCity.py
@@ -7,25 +7,95 @@ ALTO = 651
 NEGRO = (0, 0, 0)
 
 '''
-'$', -> nada
-'#', -> muro ladrillo
-'&', -> muro acero
-'.', -> pasto
+'1', -> muro ladrillo
+'2', -> muro acero
+'3', -> cesped
+'4', -> agua
+'5', -> jefe
+'.', -> nada
 '''
-#             1    2    3    4    5    6    7    8    9   10   11   12   13   14
-mapa_one = [ '$', '$', '$', '$', '$', '$', '$', '$', '$', '&', '$', '$', '$', '$', # 1
-             '$', '#', '#', '#', '$', '#', '#', '$', '#', '&', '$', '#', '#', '#', # 2
-             '$', '#', '$', '#', '$', '$', '.', '$', '#', '&', '$', '#', '$', '#', # 3
-             '$', '#', '$', '#', '$', '$', '.', '$', '#', '&', '$', '#', '#', '#', # 4
-             '$', '#', '$', '#', '$', '$', '.', '$', '#', '&', '$', '#', '$', '#', # 5
-             '$', '#', '$', '#', '$', '$', '.', '$', '#', '#', '$', '#', '$', '#', # 6
-             '$', '#', '$', '#', '$', '#', '#', '$', '#', '#', '$', '#', '$', '#'  # 7
+#             1    2    3    4    5    6    7    8    9   10   11   12   13   14   15   16   17   18   19
+#            20   21   22   23   24   25   26   27   28   29   30   31   32   33   
+mapa_one = [ '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.',
+             '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '1', '1', '2',  # 1
+
+             '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.',
+             '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '1', '1', '5', '2',  # 2
+
+             '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.',
+             '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '1', '1', '2',  # 3
+
+             '.', '.', '1', '1', '1', '1', '1', '1', '1', '.', '.', '.', '.', '.', '.', '.', '.', '1', '.',
+             '.', '2', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '2',  # 4
+
+             '.', '1', '.', '1', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.',
+             '.', '2', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '2',  # 5
+
+             '.', '3', '.', '1', '.', '.', '.', '.', '1', '1', '1', '1', '1', '1', '.', '.', '.', '1', '.',
+             '.', '2', '1', '1', '1', '1', '1', '1', '1', '.', '.', '.', '.', '2',  # 6
+
+             '.', '1', '.', '1', '.', '.', '.', '.', '.', '.', '.', '1', '.', '.', '2', '.', '.', '1', '.',
+             '.', '2', '.', '.', '.', '.', '.', '1', '.', '.', '.', '.', '.', '2',  # 7
+
+             '.', '1', '.', '1', '.', '.', '.', '.', '.', '.', '.', '1', '.', '.', '2', '.', '.', '1', '.',
+             '.', '2', '.', '.', '.', '.', '.', '1', '.', '.', '.', '.', '.', '2',  # 8
+
+             '.', '1', '.', '1', '.', '.', '.', '.', '.', '.', '.', '1', '.', '.', '2', '.', '.', '1', '.',
+             '.', '.', '.', '.', '.', '.', '.', '1', '.', '.', '.', '.', '.', '2', # 9
+
+             '.', '1', '.', '1', '.', '.', '.', '.', '.', '.', '.', '1', '.', '.', '.', '.', '.', '1', '.',
+             '.', '.', '.', '.', '.', '.', '.', '1', '.', '.', '.', '.', '.', '2', # 10
+
+             '.', '1', '.', '1', '.', '.', '.', '.', '.', '.', '.', '1', '.', '.', '2', '.', '.', '1', '.',
+             '.', '.', '.', '.', '.', '.', '.', '1', '.', '.', '.', '.', '.', '2', # 11
+
+             '.', '1', '.', '1', '.', '.', '.', '.', '.', '.', '.', '1', '.', '.', '2', '.', '.', '.', '.',
+             '.', '.', '.', '.', '.', '.', '1', '.', '.', '.', '.', '.', '.', '2', # 12
+
+             '.', '1', '.', '1', '.', '.', '.', '.', '.', '2', '2', '2', '2', '2', '2', '.', '.', '1', '1',
+             '.', '.', '.', '.', '.', '1', '.', '.', '.', '.', '.', '.', '.', '2', # 13
+
+             '.', '1', '.', '1', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '1', '1',
+             '.', '.', '.', '.', '1', '.', '.', '.', '.', '1', '.', '.', '.', '2', # 14
+
+             '.', '1', '.', '1', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.',
+             '.', '.', '.', '1', '.', '.', '.', '.', '.', '1', '.', '.', '.', '2', # 15
+
+             '.', '2', '2', '2', '.', '.', '.', '.', '.', '3', '4', '4', '4', '4', '3', '.', '.', '.', '.',
+             '.', '.', '1', '.', '.', '.', '.', '.', '.', '1', '.', '.', '.', '2', # 16
+
+             '.', '1', '.', '.', '.', '.', '.', '.', '.', '3', '3', '3', '3', '3', '3', '.', '.', '.', '.',
+             '.', '.', '1', '1', '1', '.', '.', '.', '.', '1', '.', '.', '.', '2', # 17
+
+             '.', '1', '.', '.', '.', '.', '.', '.', '.', '3', '3', '3', '3', '3', '3', '.', '.', '.', '.',
+             '.', '.', '1', '3', '1', '1', '1', '1', '1', '1', '.', '.', '.', '2', # 18
+
+             '.', '1', '.', '.', '.', '.', '.', '.', '.', '.', '3', '3', '3', '3', '.', '.', '.', '.', '.',
+             '.', '.', '2', '3', '3', '3', '3', '.', '.', '.', '.', '.', '.', '2', # 19
+
+             '.', '1', '2', '1', '1', '1', '1', '2', '2', '3', '3', '4', '4', '4', '4', '4', '4', '4', '.',
+             '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '2',  # 21
+
+             '.', '1', '2', '1', '1', '1', '1', '2', '2', '3', '3', '4', '4', '4', '4', '4', '4', '4', '.',
+             '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '2',  # 22
              ]
 
-map_columnas = 14
-map_filas = 7
+map_columnas = 33
+map_filas = 22
+
 muro_x = 31
 muro_y = 31
+
+# Creando grupos - Globales -
+muros_ladrillo = pygame.sprite.Group()
+muros_acero = pygame.sprite.Group()
+muros_duros = pygame.sprite.Group()
+bala_jp = pygame.sprite.Group()
+bala_enemy = pygame.sprite.Group()
+jefes = pygame.sprite.Group()
+enemies_static = pygame.sprite.Group()
+enemies_dinamic = pygame.sprite.Group()
+todos = pygame.sprite.Group()
 
 class Jugador(pygame.sprite.Sprite):
     def __init__(self, archivo_img):
@@ -42,7 +112,6 @@ class Jugador(pygame.sprite.Sprite):
 
     def nueva_img(self, archivo):
         self.image = pygame.image.load(archivo).convert_alpha()
-        # self.rect = self.image.get_rec()
 
     def update(self):
         desplx = self.rect.x
@@ -95,6 +164,42 @@ class Jugador(pygame.sprite.Sprite):
                     self.rect.bottom = m.rect.top
         self.dir = 0
 
+class Bala_Enemigos(pygame.sprite.Sprite):
+    def __init__(self,archivo_img,pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(archivo_img).convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+        self.dir = 0
+        self.var_y = 1
+
+    def nueva_img(self, archivo):
+        self.image = pygame.image.load(archivo).convert_alpha()
+
+    def update(self):
+        desply = self.rect.y
+        self.rect.y = self.rect.y + self.var_y
+        self.nueva_img('bala_down.png')
+
+class EnemyStatic(pygame.sprite.Sprite):
+    def __init__(self, archivo_img, pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(archivo_img).convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+        self.temp = random.randint(200,500)
+
+    def update(self):
+        self.temp = self.temp - 1
+        if self.temp == 0:
+            bala = Bala_Enemigos('bala_down.png', [64, 5])
+            bala.dir = 4
+            bala_enemy.add(bala)
+            todos.add(bala)
+            self.temp = random.randint(200,500)
+
 class Muro(pygame.sprite.Sprite):
     def __init__(self, archivo_img, pos):
         pygame.sprite.Sprite.__init__(self)
@@ -103,7 +208,7 @@ class Muro(pygame.sprite.Sprite):
         self.rect.x = pos[0]
         self.rect.y = pos[1]
 
-class Bala(pygame.sprite.Sprite):
+class Bala_Jugador(pygame.sprite.Sprite):
     def __init__(self,archivo_img,pos):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(archivo_img).convert_alpha()
@@ -120,12 +225,6 @@ class Bala(pygame.sprite.Sprite):
     def update(self):
         desplx = self.rect.x
         desply = self.rect.y
-        '''
-        dir = 1 derecha
-        dir = 2 izquierda
-        dir = 3 arriba
-        dir = 4 abajo
-        '''
         if self.dir == 1:
             self.rect.x = self.rect.x + self.var_x
             self.nueva_img('bala_right.png')
@@ -139,21 +238,14 @@ class Bala(pygame.sprite.Sprite):
             self.rect.y = self.rect.y + self.var_y
             self.nueva_img('bala_down.png')
 
+
 if __name__ == '__main__':
     # Inicializar pygame
     pygame.init()
     # Inicializar pantalla
     pantalla = pygame.display.set_mode([ANCHO, ALTO])
 
-    # Creando grupos
-    muros = pygame.sprite.Group()
-    enemies_estatic = pygame.sprite.Group()
-    enemies_dinamic = pygame.sprite.Group()
-    balas = pygame.sprite.Group()
-    todos = pygame.sprite.Group()
-
-    # Leer mapa
-    # x y -> posiciones de la imagen
+    # Leer mapa (x y -> posiciones de la imagen)
     inicio = y = x_i = y_j = 0
     fin = map_columnas
     for k in range(map_filas):
@@ -162,20 +254,29 @@ if __name__ == '__main__':
         # print fila
         for element in fila:
             x = 0 + (x_i * muro_x)
-            if element == '#':
-                muro_ladrillo = Muro('muro_ladrillo.png', [x,y])
-                todos.add(muro_ladrillo)
-                muros.add(muro_ladrillo)
-                #print [x,y]
-            elif element == '&':
-                muro_acero = Muro('muro_acero.png', [x,y])
-                todos.add(muro_acero)
-                muros.add(muro_acero)
-            elif element == '.':
+            if element == '1':
+                ladrillo = Muro('muro_ladrillo.png', [x,y])
+                muros_ladrillo.add(ladrillo)
+                muros_duros.add(ladrillo)
+                todos.add(ladrillo)
+            elif element == '2':
+                acero = Muro('muro_acero.png', [x,y])
+                muros_acero.add(acero)
+                muros_duros.add(acero)
+                todos.add(acero)
+            elif element == '3':
                 cesped = Muro('cesped.png', [x,y])
                 todos.add(cesped)
-                muros.add(cesped)
-            elif element == '$':
+                muros_ladrillo.add(cesped)
+            elif element == '4':
+                agua = Muro('agua.png', [x,y])
+                todos.add(agua)
+                muros_ladrillo.add(agua)
+            elif element == '5':
+                jefe = Muro('jefe.png', [x,y])
+                todos.add(jefe)
+                jefes.add(jefe)
+            elif element == '.':
                 pass
                 # print "numeral"
             x_i = x_i + 1
@@ -185,18 +286,19 @@ if __name__ == '__main__':
         fin = fin + map_columnas
 
     jp = Jugador('tanquedown.png')
-    jp.muros = muros
+    jp.muros = muros_duros
     todos.add(jp)
 
-    # # Nivel 1
-    # static = 2
-    # dinamic = 4
-
+    static = EnemyStatic('enemystatic_down.png', [64, 5])
+    enemies_static.add(static)
+    todos.add(static)
+    num_enemy = 5
     fin = False
     while not fin:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 fin = True
+            num_enemy = num_enemy - 1
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     jp.dir = 1
@@ -212,47 +314,61 @@ if __name__ == '__main__':
                     jp.orientacion = 4
                 if event.key == pygame.K_SPACE:
                     if jp.orientacion == 1:
-                        bala = Bala('bala_right.png', [jp.rect.x, jp.rect.y])
-                        print jp.rect.x,"right",jp.rect.y,'\n'
+                        bala = Bala_Jugador('bala_right.png', [jp.rect.x, jp.rect.y])
                         bala.dir = 1
-                        balas.add(bala)
+                        bala_jp.add(bala)
                         todos.add(bala)
                     elif jp.orientacion == 2:
-                        bala = Bala('bala_left.png', [jp.rect.x, jp.rect.y])
-                        print jp.rect.x,"left",jp.rect.y,'\n'
+                        bala = Bala_Jugador('bala_left.png', [jp.rect.x, jp.rect.y])
                         bala.dir = 2
-                        balas.add(bala)
+                        bala_jp.add(bala)
                         todos.add(bala)
                     elif jp.orientacion == 3:
-                        bala = Bala('bala_up.png', [jp.rect.x, jp.rect.y])
-                        print jp.rect.x,"up",jp.rect.y,'\n'
+                        bala = Bala_Jugador('bala_up.png', [jp.rect.x, jp.rect.y])
                         bala.dir = 3
-                        balas.add(bala)
+                        bala_jp.add(bala)
                         todos.add(bala)
                     elif jp.orientacion == 4:
-                        bala = Bala('bala_down.png', [jp.rect.x, jp.rect.y])
-                        print jp.rect.x,"down",jp.rect.y,'\n'
+                        bala = Bala_Jugador('bala_down.png', [jp.rect.x, jp.rect.y])
                         bala.dir = 4
-                        balas.add(bala)
+                        bala_jp.add(bala)
                         todos.add(bala)
 
-        for bala in balas:
-            l_imp = pygame.sprite.spritecollide(bala, muros, True)
-            for e in l_imp:
-                balas.remove(bala)
+        for bala in bala_jp:
+            col_ladrillos = pygame.sprite.spritecollide(bala, muros_ladrillo, True)
+            col_aceros = pygame.sprite.spritecollide(bala, muros_acero, False)
+            for e in col_ladrillos:
+                bala_jp.remove(bala)
+                todos.remove(bala)
+            for i in col_aceros:
+                bala_jp.remove(bala)
                 todos.remove(bala)
 
-        jp.muros = muros
+        for bala in bala_enemy:
+            col_ladrillos = pygame.sprite.spritecollide(bala, muros_ladrillo, True)
+            col_aceros = pygame.sprite.spritecollide(bala, muros_acero, False)
+            for e in col_ladrillos:
+                bala_enemy.remove(bala)
+                todos.remove(bala)
+            for i in col_aceros:
+                bala_enemy.remove(bala)
+                todos.remove(bala)
+
+        for bala in bala_enemy:
+            col_balas = pygame.sprite.spritecollide(bala, bala_jp, False)
+            for e in col_balas:
+                bala_enemy.remove(bala)
+                todos.remove(bala)
+                for bala in bala_jp:
+                    bala_jp.remove(bala)
+                    todos.remove(bala)
+
+        jp.muros = muros_duros
         todos.add(jp)
         pantalla.fill(NEGRO)
         todos.update()
         todos.draw(pantalla)
         pygame.display.flip()
-
-        # dir = 1 derecha
-        # dir = 2 izquierda
-        # dir = 3 arriba
-        # dir = 4 abajo
 
         # print "left",e.rect.left
         # print "right",e.rect.right
